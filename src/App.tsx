a2541a9a83bc8467a4b8008f058d9036254d52ea
Jay Savlani <jay.savlani@brainvire.com>
2023-08-22 17:58:06 +0530

    Changed code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function App () {
-  const a = true
-  const b = true
+  const a = true;
+  const b = true;
   return (
-    <>
+    <div style={{ padding: '5px' }}>
       {a ?
         <h1>A is True</h1> : b ? <h1>B is True</h1> : <>A and B both are false</>}
-    </>
+    </div>
   );
 }
 
