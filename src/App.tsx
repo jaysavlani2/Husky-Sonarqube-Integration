640bb40a8dfb9026f17180a8e91dc4eb543b1fad
Jay Savlani <jay.savlani@brainvire.com>
2023-08-22 17:21:06 +0530

    Removed env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 function App () {
+  const a = true
+  const b = true
   return (
-    <></>
+    <>
+      {a ?
+        <h1>A is True</h1> : b ? <h1>B is True</h1> : <>A and B both are false</>}
+    </>
   );
 }
 
